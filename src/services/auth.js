b9651de04ce8f1e371fa419fa7b3d3e9a3c52141
pavan6cs <bhavanas904@gmail.com>
2023-07-01 17:39:14 +0530

    fixed: auth issue

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const loginService = async(username, password) =>{ 
+    
     return await axios.post("/api/auth/login", {username, password})
 }
 export const signupService = async(firstName, lastName, username, password,avatarURL) =>{
